5976f15082ef789008d71cd95de465043794812e
chimek2107 <b.chimkowski@tuta.io>
2022-12-13 20:17:05 +0100

    jeden

--- foo.hpp
+++ foo.hpp
@@ -4,9 +4,23 @@
 
 #include <list>
 #include <vector>
+#include <iostream>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+	// Twoja implementacja tutaj
+	
+	std::vector< char > v;
+
+	for (Human &it_people : people) 
+	{
+		it_people.birthday();
+		if (it_people.isMonster()) 
+			v.emplace( v.begin(), 'n' );
+		else 
+			v.emplace( v.begin(), 'y' );
+	}
+
+
+	return v;
 }
